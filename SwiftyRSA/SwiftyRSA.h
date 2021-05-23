054323b307b4cde010452f4d1eb72964bbaa448c
Albert Leal <albert.leal@es.nestle.com>
2021-05-24 00:01:37 +0200

    Adding MacOS

---
+++ SwiftyRSA/SwiftyRSA.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRSA.h
+//  SwiftyRSA
+//
+//  Created by ESLealAl on 23/5/21.
+//  Copyright © 2021 Scoop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyRSA.
+FOUNDATION_EXPORT double SwiftyRSAVersionNumber;
+
+//! Project version string for SwiftyRSA.
+FOUNDATION_EXPORT const unsigned char SwiftyRSAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRSA/PublicHeader.h>
+
+
